964ed72dd85afab377f542a5481aaee35e2dd177
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2024-02-15 21:54:26 -0500

    Added new files...

---
+++ Leetcode 領域.py
@@ -0,0 +1,18 @@
+# noinspection PyUnresolvedReferences
+from collections import defaultdict, deque
+from typing import Optional, List
+
+FUNC_NAME, PARAMS = "func", []
+
+
+# noinspection PyPep8Naming, PyMethodMayBeStatic
+class Solution:
+    def __init__(self, dummy: Optional[List] = None):
+        self.dummy = dummy
+
+    def func(self, par):
+        return par
+
+
+if __name__ == "__main__":
+    print(getattr(Solution(), FUNC_NAME)(*PARAMS))
